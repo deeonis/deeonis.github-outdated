1c0faf622f3d4d3f3b0ad7c746285e2d1eb62524
deeonis <kuznecov@yandex-team.ru>
2014-07-21 18:19:25 +0400

    Поправил код, удалил лишнее, правильно назвал переменные

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
-var identification = function(owner)
-	{	
-	if (owner === "Denis")
-		{
+function identification(owner) {	
+	if (owner === "Denis") {
 		var access = [1,"access is allowed"];
-		}
-	else
-		{
+	}
+	else {
 		var access = [0,"access denied"];
-		}
+	}
 	return access;
-	} 
-var kp = function()
-	{
+} 
+
+function checkEnterAndSubmit() {
 	if (window.event.keyCode == 13) {
 		buttonclick();
 	}
-	}
-var buttonclick = function()
-	{
-	var input = document.getElementById('search'); 
-	var strOwner = input.value;
-	var access = identification(strOwner);
-	console.log(access);
+}
 
-	var myBody = document.getElementsByTagName('body')[0];
+function buttonclick() {
+	var input = document.getElementById('name'); 
+	var name = input.value;
+	var access = identification(name);
 	var newdiv = document.createElement('div');
-	
-	myBody.appendChild(newdiv);
-	newdiv.innerHTML = access[1];
-
-	var par = document.getElementById('m');
+	var par = document.getElementById('output');
 	par.insertBefore(newdiv, par.firstChild);
-	newdiv.innerHTML = ("<p>" + "</p>" + access[1]);
-	if (access[0] == 0)
-		{
+	newdiv.innerHTML = ("<p></p>" + access[1]);
+	if (access[0] == 0) {
 		newdiv.style.color = '#FF0000';
-		}
-	else
-		{
-		newdiv.style.color = '#00FF00'
-		}
 	}
+	else {
+		newdiv.style.color = '#00FF00'
+	}
+}
